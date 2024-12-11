6203fa58b4d3b0af6c4acf9e02f14c3a84564fce
Clara Regula <clara@Claras-MacBook-Pro-2.local>
2024-12-10 23:12:46 -0500

    installing aws sdk v2 - need to install v3

---
+++ client/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// import './css/input.css';
+require("dotenv").config();
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+s3.listBuckets((err, data) => {
+    if (err)
+        console.log(err, err.stack);
+    else
+        console.log(data);
+});
--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,10 @@
-import './css/input.css';
+// import './css/input.css';
+require("dotenv").config();
+
+const AWS = require("aws-sdk")
+const s3 = new AWS.S3();
+
+s3.listBuckets((err: Error, data: object) => {
+    if (err) console.log(err, err.stack)
+        else console.log(data)
+});
